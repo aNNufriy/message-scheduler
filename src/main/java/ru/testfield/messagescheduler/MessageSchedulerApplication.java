b744b6e663515c87211fde4d7d01970fe395c608
Alexander Schedrov <schedrow@gmail.com>
2021-02-11 16:26:45 +0300

    initial: start.spring.io

---
+++ src/main/java/ru/testfield/messagescheduler/MessageSchedulerApplication.java
@@ -0,0 +1,13 @@
+package ru.testfield.messagescheduler;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MessageSchedulerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MessageSchedulerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/ru/testfield/messagescheduler/MessageSchedulerApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.testfield.messagescheduler;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MessageSchedulerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
